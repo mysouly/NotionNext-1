2da09b63d714399e8c71e898f0509409b6bd0222
mysouly000 <100502822+mysouly000@users.noreply.github.com>
2022-03-09 00:07:08 +0800

    Update config_next.js

--- themes/NEXT/config_next.js
+++ themes/NEXT/config_next.js
@@ -23,7 +23,7 @@ const CONFIG_NEXT = {
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
-  MENU_ABOUT: true, // 显示关于
+  MENU_ABOUT: false, // 显示关于
   MENU_SEARCH: true, // 显示搜索
 
   // 悬浮挂件
